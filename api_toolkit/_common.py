9e09964ceff3b22cbdf368cc7f738564b3f4af11
AnufrievSY <russianeagleowl@gmail.com>
2026-02-05 10:21:43 +0500

    ref: ранее реализованный вайб-код возвращал полученный словарь в строков виде, но это может ломать логику работы модуля, так что пускай лучше срабатывают исключения

--- api_toolkit/_common.py
+++ api_toolkit/_common.py
@@ -5,8 +5,5 @@ def to_hashkey(**kwargs) -> str:
     # Превращаем dict в стабильную строку:
     # sort_keys=True — гарантирует одинаковый порядок ключей
     # separators — убираем лишние пробелы чтобы строка всегда была одинаковой
-    try:
-        s = json.dumps(kwargs, ensure_ascii=False, sort_keys=True, separators=(",", ":"))
-    except Exception:
-        return repr(kwargs)
+    s = json.dumps(kwargs, ensure_ascii=False, sort_keys=True, separators=(",", ":"))
     return hashlib.md5(s.encode("utf-8")).hexdigest()
